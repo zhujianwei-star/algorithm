77ddeb85490163dabfb01d13e3d9de7966b28f32
ZhuJW <zhujianweiiove@gmail.com>
2024-05-06 14:23:43 +0800

    增加L134的定义

---
+++ leetcode/src/main/java/org/example/leetcode/l134/L134.java
@@ -0,0 +1,22 @@
+package org.example.leetcode.l134;
+
+/**
+ * @description: 第L134题 - 加油站
+ * @question: <a href="https://leetcode-cn.com/problems/gas-station/">...</a>
+ * @answer:
+ */
+public class L134 {
+
+    public static void main(String[] args) {
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        int result = canCompleteCircuit(gas, cost);
+        System.out.println(result);
+    }
+
+    // TODO: 未解出来
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        return -1;
+    }
+
+}
