067c2cdd0dbe4a800261fc801048bf9854da5aef
ZhuJW <zhujianweiiove@gmail.com>
2024-08-15 10:52:57 +0800

    力扣第L392题 - 判断子序列

--- leetcode/src/main/java/org/example/leetcode/l392/L392.java
+++ leetcode/src/main/java/org/example/leetcode/l392/L392.java
@@ -1,5 +1,7 @@
 package org.example.leetcode.l392;
 
+import java.util.Objects;
+
 /**
  * @description: 第L392题 - 判断子序列
  * @question: <a href="https://leetcode-cn.com/problems/is-subsequence">...</a>
@@ -14,6 +16,21 @@ public class L392 {
     }
 
     private static boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        if (t.isEmpty()) {
+            return false;
+        }
+        int sLen = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (Objects.equals(t.charAt(i), s.charAt(sLen))) {
+                sLen++;
+            }
+            if (sLen == s.length()) {
+                return true;
+            }
+        }
         return false;
     }
 
